30301c03d054dbc4fe7d6673553812e9f93adced
MichaelBG <michaelbekele28@gmail.com>
2022-04-12 10:29:13 +0300

    query and display blog posts

--- wp-content/themes/mytheme/front-page.php
+++ wp-content/themes/mytheme/front-page.php
@@ -170,16 +170,18 @@
 											?>
 											<div class="col-6 col-12-small">
 												<section class="box">
-													<a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
+													<a href="<?php the_permalink(); ?>" class="image featured">
+														<?php the_post_thumbnail('home-featured');?>
+													</a>
 													<header>
-														<h3>Aptent veroeros aliquam</h3>
-														<p>Posted 45 minutes ago</p>
+														<h3> <?php the_title()?> </h3>
+														<p>Posted on <?php the_date(); ?> at <?php the_time(); ?> </p>
 													</header>
-													<p>Lorem ipsum dolor sit amet sit veroeros sed et blandit consequat sed veroeros lorem et blandit adipiscing feugiat phasellus tempus hendrerit, tortor vitae mattis tempor, sapien sem feugiat sapien, id suscipit magna felis nec elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos lorem ipsum dolor sit amet.</p>
+													<?php the_excerpt() ?>
 													<footer>
 														<ul class="actions">
-															<li><a href="#" class="button icon solid fa-file-alt">Continue Reading</a></li>
-															<li><a href="#" class="button alt icon solid fa-comment">33 comments</a></li>
+															<li><a href="<?php the_permalink(); ?>" class="button icon solid fa-file-alt">Continue Reading</a></li>
+															<li><a href="<?php comments_link(); ?>" class="button alt icon solid fa-comment"><?php echo get_comments_number();?> comments</a></li>
 														</ul>
 													</footer>
 												</section>
